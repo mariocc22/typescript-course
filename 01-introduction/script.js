efbdb32302cdbc5edc68a018297c5099643c9ad2
Mario Cesena <mceseahurtado00@mylangara.ca>
2024-01-06 18:02:17 -0800

    init: initial commit

---
+++ 01-introduction/script.js
@@ -0,0 +1,6 @@
+var num1 = 1;
+var num2 = 2;
+function addValues(a, b) {
+    console.log("the sum is : ".concat(a + b));
+}
+addValues(num1, num2);
---
+++ 01-introduction/script.ts
@@ -0,0 +1,8 @@
+const num1 = 1;
+const num2 = 2;
+
+function addValues(a: number, b: number) {
+  console.log(`the sum is : ${a + b}`);
+}
+
+addValues(num1, num2);
